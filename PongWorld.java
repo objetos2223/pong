8861f6d53fc80de9c3d792f4f1e350787df4ce74
Programación Orientada a Objetos <objetos2223@gmail.com>
2018-10-10 08:17:23 -0500

    Version incial del juego.

---
+++ PongWorld.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class PongWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class PongWorld  extends World
+{
+
+    /**
+     * Constructor for objects of class PongWorld.
+     * 
+     */
+    public PongWorld()
+    {    
+        // Create a new world with 20x20 cells with a cell size of 10x10 pixels.
+        super(700, 500, 1);
+        
+        addObject(new Paddle(), 350, 30);
+        addObject(new Paddle(), 350, 470);
+        addObject(new Ball(), 350, 250);
+        
+    }
+}
