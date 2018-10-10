0429cab4b9bbcab01c0bedd03fbc04882279294e
tecweb2225 <tecweb2225@gmail.com>
2018-10-10 08:25:35 -0500

    Player1 and Player2 added.

--- PongWorld.java
+++ PongWorld.java
@@ -18,8 +18,8 @@ public class PongWorld  extends World
         // Create a new world with 20x20 cells with a cell size of 10x10 pixels.
         super(700, 500, 1);
         
-        addObject(new Paddle(), 350, 30);
-        addObject(new Paddle(), 350, 470);
+        addObject(new Player1(), 350, 30);
+        addObject(new Player2(), 350, 470);
         addObject(new Ball(), 350, 250);
         
     }
