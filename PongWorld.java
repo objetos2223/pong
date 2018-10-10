31193c3211701bd77e7607b896e23edac929dd99
tecweb2225 <tecweb2225@gmail.com>
2018-10-10 09:53:54 -0500

    Comentario en World

--- PongWorld.java
+++ PongWorld.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class PongWorld  extends World
 {
-
+    // comentario en World
     /**
      * Constructor for objects of class PongWorld.
      * 
